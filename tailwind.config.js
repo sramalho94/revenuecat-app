044208348539d8fd2e4546707fb5ff1eacb24658
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-09 17:00:25 -0400

    added custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './components/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'papa-orange': '#E5962D'
+      }
+    }
   },
   plugins: []
 }
