e46018594f90b8f52084239d67cbb68e24f12dac
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-09 14:58:57 -0400

    set up react-navigation configuration and made screens directory

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View, SafeAreaView } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/HomeScreen'
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <SafeAreaView className="flex flex-row">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
