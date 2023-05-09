7959d9e1b16d1d1dfb879230a1fd460a79cf1860
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-09 14:41:05 -0400

    Revert "config for tailwind and typescript"
    This reverts commit 26e94be95182cbee07f04642f9bf90ddcf9947c1.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View, SafeAreaView } from 'react-native'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <SafeAreaView className="flex flex-row justify-center">
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </View>
   )
 }
